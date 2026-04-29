7ca1d5d9d1c2e510659277eceeb6f02df6aa1166
sneu1814 <snehabharmal18@gmail.com>
2026-04-29 17:32:09 +0000

    Added Selenium Login Test

---
+++ QA_Automation-Project/automation/src/test/java/com/qa/LoginTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void validLoginTest() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        System.out.println("Login Successful");
+
+        driver.quit();
+    }
+}
